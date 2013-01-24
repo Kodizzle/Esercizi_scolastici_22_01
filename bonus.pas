a3e8b942db41f481a4f752ef37fd9a8034f05e1f
Kodizzle <not.kodizzle@gmail.com>
2013-01-24 20:42:45 +0100

    TROLOLOLOL

--- bonus.pas
+++ bonus.pas
@@ -1,4 +1,24 @@
 program bonus;
+(*
+ * Copyleft none (c) 2013 Aperture Laboratories, Inc. All rights free.
+ *
+ * @TROLLING_LICENSE
+ * This file contains Original code of this,if you want pirate free, 
+ * You are a pirate.
+ *
+ *
+ *
+ * The latest innovation in human-developing interaction. Our scientist have
+ * shown in extensive tests that humans operating our fail project  on their
+ * PC experience 33% more joy in life while smimultaneosly the likelihood of
+ * being maimed or killed by falling pace debris is significantly reduced.
+ * Also preliminary human trial indicate that it might offer some protection
+ * against lethal dosages of gamma radiation and blows to  the head  with  a
+ * rodlike object
+ *
+ * Kodizzle <not(dot)kodizzle(at)gmail(dot)com>
+ * 
+ *)
 
 const m = 10;
 
